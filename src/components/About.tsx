9f516d0ff590ff6271dfd09ecd82859d14dccacb
Abdullah3132 <shaikh.abdullah3132@gmail.com>
2026-03-26 16:46:20 +0500

    Initial commit

---
+++ src/components/About.tsx
@@ -0,0 +1,19 @@
+import "./styles/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-me">
+        <h3 className="title">About Me</h3>
+        <p className="para">
+          I'm a Webdeveloper with 3 years of experience in Wordpress/shopify and other CMS
+          platforms. I'm passionate about building websites that are both
+          beautiful and functional. I'm also a bit of a perfectionist, so I
+          always strive to deliver the best possible results.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
